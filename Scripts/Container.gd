02255a1ef129fd6a86e965584dfc5af8dbb22f75
Daniel Augusto <daniel.aug.souza@gmail.com>
2023-01-20 20:48:51 -0300

    Upd show more elements

--- Scripts/Container.gd
+++ Scripts/Container.gd
@@ -15,25 +15,41 @@ func build_container(group, parent):
       c.name = g
       c.set_size(Vector2(180,0))
       parent.add_child(c)
-      var l = Label.new()
-      l.name = "+ " + g
-      l.text = "+ " + g
+      var l0 = Label.new()
       var l1 = Label.new()
       var l2 = Label.new()
       var l3 = Label.new()
+      l0.name = "+ " + g
+      l0.text = "+ " + g
       l1.name = "Amount"
       l1.text = "A"
       l2.name = "SellPrice"
       l2.text = "S"
       l3.name = "BuyPrice"
       l3.text = "B"
-      c.add_child(l)
+      c.add_child(l0)
       c.add_child(l1)
       c.add_child(l2)
       c.add_child(l3)
-
-      for el in group[g][1]:
-        var l_el = Label.new()
-        l_el.name = el
-        l_el.text = el
-        c.add_child(l_el)
+      
+      var subgroup = group[g][1]
+      #print(subgroup)
+      for el in subgroup:
+        if subgroup[el][0]:
+          var el0 = Label.new()
+          var el1 = Label.new()
+          var el2 = Label.new()
+          var el3 = Label.new()
+          el0.name = "--" + el
+          el0.text = "--" + el
+          el1.name = "ItemAmount"
+          el1.text = str(subgroup[el][1])
+          el2.name = "ItemSellPrice"
+          el2.text = str(subgroup[el][2])
+          el3.name = "ItemBuyPrice"
+          el3.text = str(subgroup[el][3])
+          c.add_child(el0)
+          c.add_child(el1)
+          c.add_child(el2)
+          c.add_child(el3)
+          print(subgroup[el])
--- Scripts/Elements.gd
+++ Scripts/Elements.gd
@@ -1,9 +1,9 @@
 extends Node
 
 ## Individual Tables
-# {<name>: [<enabled>, <quantity>, <buy_price>, <sell_price>]}
-var market = {"chocoCoins": [true, 0, 0.1, 0.03], "beans": [false, 0, 5, 1.0]}
-var clothes = {"shirt": [false, 0, 0.1, 0.03], "jeans": [false, 0, 5, 1.0]}
+# {<name>: [<enabled>, <quantity>, <sell_price>, <buy_price>]}
+var market = {"chocoCoins": [true, 0, 0.03, 0.1], "beans": [false, 0, 1.0, 5]}
+var clothes = {"shirt": [false, 0, 0.03, 0.1], "jeans": [false, 0, 1.0, 5]}
 
 ## Group Tables
 # <Name>: [<dict>, <enabled>]
