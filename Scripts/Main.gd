55ebe944ca67d56cf39aa9cd3030301dd0906be7
Daniel Augusto <daniel.aug.souza@gmail.com>
2023-01-20 17:27:56 -0300

    New working basic save-load

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,21 +1,33 @@
 extends Control
 
 const coin = preload("res://Scripts/Coin.gd")
-var coins = 0
+const saveLoadGame = preload("res://Scripts/SaveLoad.gd")
+
+var coins = coin.new()
+var saveLoad = saveLoadGame.new()
 var time = 0
 var timeLimit = 1
+var timeSave = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	coins = coin.new()
-	get_node("./Coins").text = str("%.2f" % coins.show()) + " C"
+	saveLoad.loadGame()
+	print(saveLoad.game_data.coins[0])
+	coins.current_value = float(saveLoad.game_data.coins[0])
+	get_node("MainScrollContainer/MainVBoxContainer/Coins").text = str("%.2f" % coins.show()) + " C"
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	# Make things happen each SECOND
-	time = time + delta
+	# Make things happen EACH SECOND
+	time += delta
 	if time > timeLimit:
 		coins.add()
-		get_node("./Coins").text = str("%.2f" % coins.show()) + " C"
+		get_node("MainScrollContainer/MainVBoxContainer/Coins").text = str("%.2f" % coins.show()) + " C"
 		time = 0
 	
+	# Make things happen EACH 5 SECONDS
+	timeSave += delta
+	if timeSave >= 5:
+		saveLoad.saveGame(coins)
+		timeSave = 0
---
+++ Scripts/SaveLoad.gd
@@ -0,0 +1,34 @@
+var path="user://save_game.json";
+var passFile="hdheinfhrjebfggu3lwqdww73iendymwhcgcfwksuidgb"
+
+
+#Any number of variables of different types
+var game_data = {}
+
+func saveGame(coins):
+	var file = File.new()
+
+	game_data = {
+		"coins": [coins.current_value, coins.coin_add]
+	}
+
+	file.open_encrypted_with_pass(path, File.WRITE, passFile)
+	file.store_var(to_json(game_data), true)
+	print(game_data)
+	file.close()
+	print("Saved")
+	pass
+
+func loadGame():
+	var file = File.new()
+
+	if file.file_exists(path):
+		file.open_encrypted_with_pass(path, File.READ, passFile)
+		var loadParam = parse_json(file.get_var(true));
+		file.close()
+
+		if loadParam!=null:
+			game_data = loadParam;
+			print("Loaded")
+			pass;
+	pass;
