09421b02e23f35dc1d54b9866f274ac4d6cc5aa7
Daniel Augusto <daniel.aug.souza@gmail.com>
2023-01-20 02:51:04 -0300

    Rm flat_upg

--- Scripts/Coin.gd
+++ Scripts/Coin.gd
@@ -2,14 +2,9 @@ extends Node
 
 var current_value = 0
 var coin_add = 0.01
-var flat_upg = 0.01
-
 
 func add():
 	current_value += coin_add
 
 func show():
 	return current_value
-
-func upgrade():
-	current_value += flat_upg
