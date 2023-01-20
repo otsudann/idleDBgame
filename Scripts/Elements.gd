8340e26ebe4fbf3b4a5dc798344bf7a154dfd875
Daniel Augusto <daniel.aug.souza@gmail.com>
2023-01-20 17:27:11 -0300

    Upd tables turned to false on start

--- Scripts/Elements.gd
+++ Scripts/Elements.gd
@@ -4,12 +4,13 @@ const coin = preload("res://Scripts/Coin.gd")
 onready var coins = coin.new()
 
 ## Tables and Values
-# {<name>: [<quantity>, <buy_price>, <sell_price>]}
-var market = {"chocoCoins": [0, 0.1, 0.03], "beans": [0, 5, 5.05]}
-var clothes = {"shirt": [0, 0.1, 0.03], "jeans": [0, 5, 5.05]}
+# {<name>: [<quantity>, <buy_price>, <sell_price>], <enabled>}
+var market = {"chocoCoins": [0, 0.1, 0.03, false], "beans": [0, 5, 5.05, false]}
+var clothes = {"shirt": [0, 0.1, 0.03, false], "jeans": [0, 5, 5.05, false]}
 
-# All elements
-var elements = {"Market":market, "Clothes":clothes}
+## All elements
+# <Name>: [<dict>, <enabled>]
+var elements = {"Market":[market, false], "Clothes":[clothes, false]}
 
 # Ex: upd_val(elements['Market'])
 func update_values(element, name):
